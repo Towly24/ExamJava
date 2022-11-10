6622ab1d01a6dd1ef112af3b6ad8d74e57676b60
towly249 <caonamtq249@gmail.com>
2022-11-10 18:57:24 +0700

    10/11

---
+++ src/HomeController.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+public class HomeController {
+    public TableColumn tdPhone;
+    public TableColumn tdAdress;
+    public TableColumn tdName;
+    public TableColumn tdId;
+    public TableView tbStudent;
+
+    public void AddStudent(ActionEvent actionEvent) throws Exception {
+        Parent addStudent = FXMLLoader.load(getClass().getResource("addStudent/addStudent.fxml"));
+        Main.rootStage.setTitle("AddStudent");
+        Main.rootStage.setScene(new Scene(addStudent,800,600));
+    }
+}
---
+++ src/addStudent/AddStudentController.java
@@ -0,0 +1,17 @@
+package addStudent;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class AddStudentController {
+    public TextField txPhone;
+    public TextField txName;
+    public TextField txAddress;
+    public TextField txId;
+
+    public void add(ActionEvent actionEvent) {
+    }
+
+    public void back(ActionEvent actionEvent) {
+    }
+}
